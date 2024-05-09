2796ad21b4c76ff3429dc9017074b5a53d7edf0b
USER <devanshsaxena12b1@gmail.com>
2024-05-09 10:06:00 +0530

    Testing Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CodeSprint Editor");
+        System.out.println("Testing on Github.com");
     }
 }
