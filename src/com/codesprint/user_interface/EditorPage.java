5936ab09f12f65982392be51747839b934b244da
USER <devanshsaxena12b1@gmail.com>
2024-05-12 16:22:51 +0530

    Testing Project

--- src/com/codesprint/user_interface/EditorPage.java
+++ src/com/codesprint/user_interface/EditorPage.java
@@ -1,10 +1,12 @@
 package com.codesprint.user_interface;
 
-import com.codesprint.constants.Constants;
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.jidesoft.swing.JideTabbedPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JTextArea;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
 
@@ -12,7 +14,7 @@ public class EditorPage{
     public EditorPage(){
         FlatDarkLaf.setup();
         JFrame frame=new JFrame();
-        frame.setIconImage(Constants.mainIcon.getImage());
+        frame.setIconImage(new FlatSVGIcon(getClass().getResource("/mainIcon.svg")).getImage());
         JMenuBar menuBar=new JMenuBar();
         menuBar.setBorder(new MatteBorder(0,0,1,0,Color.gray));
         JMenu file=new JMenu("File");
@@ -26,6 +28,16 @@ public class EditorPage{
         frame.setSize(1280,786);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JideTabbedPane tabbedPane = getJideTabbedPane();
+        frame.add(tabbedPane);
         frame.setVisible(true);
+    }
+
+    private static JideTabbedPane getJideTabbedPane() {
+        JideTabbedPane tabbedPane=new JideTabbedPane();
+        tabbedPane.setShowCloseButtonOnTab(true);
+        for(int loop=1;loop<=10;loop++)
+            tabbedPane.add("Main"+loop+".java ",new JTextArea());
+        return tabbedPane;
     }
 }
